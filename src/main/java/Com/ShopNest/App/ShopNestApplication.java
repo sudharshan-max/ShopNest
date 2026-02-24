4ac90c4097eb28c1406ed0c3999290a9673ba075
sudharshan <sudharshangudimi@gmail.com>
2026-02-24 12:39:43 +0530

    initial commit

---
+++ src/main/java/Com/ShopNest/App/ShopNestApplication.java
@@ -0,0 +1,13 @@
+package Com.ShopNest.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopNestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopNestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Com/ShopNest/App/ShopNestApplicationTests.java
@@ -0,0 +1,13 @@
+package Com.ShopNest.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopNestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
